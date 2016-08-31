78be24a6412752b29dfc81608b2d3fbc7a1056dd
Mariana Angelova <Mariana Angelova>
2016-08-31 19:56:07 +0300

    First commit

---
+++ MVC-Blog/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using MVC_Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new ApplicationDbContext();
+            var posts = db.Posts.OrderByDescending(p => p.Date).Take(3);
+            return View(posts.ToList());
+
+        }
+
+
+        public ActionResult Blog()
+        {
+            return View("Blog");
+        }
+
+        public ActionResult About()
+        {
+            return View("About");
+        }
+
+        public ActionResult Contacts()
+        {
+            return View("Contacts");
+        }
+
+        public ActionResult UserProfile()
+        {
+            return View("UserProfile");
+        }
+    }
+}
---
+++ MVC-Blog/Models/Comment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Blog.Models
+{
+    public class Comment
+    {
+        public Comment()
+        {
+            this.Date = DateTime.Now;
+        }
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string Text { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public Post Post { get; set; }
+        public int Post_Id { get; set; }
+    }
+}
